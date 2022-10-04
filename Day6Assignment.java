8e11d7c2e25f6603b7bf63000bc8621f0b724d8b
vaidya0303 <vaidya0304@gmail.com>
2022-10-04 11:07:01 +0530

    perfect number

--- Day6Assignment.java
+++ Day6Assignment.java
@@ -1,33 +1,30 @@
+
 import java.util.Scanner;
 
-public class Day6Assignment 
-{
-	public static void main(String[] args) 
-	{
-		char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
-		//ask user for number of characters in the coupon
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Entre a number of characters in coupon");
-		int nch = sc.nextInt();
-		//ask how many coupons user wants
-		System.out.println("Entre a number coupons required");
-		int ncp = sc.nextInt();
-		
-		int i = 0;
-		while(i<ncp) {
-			String couponCode="";
-			//StringBuffer sb=new StringBuffer();
-			i++;
-			int j=0;
-			while (j<nch)//generate a random index and append the character of that index to coupon code
-			{
-				//generate a random index for character array
-				int random=(int) (Math.random()*1000 % 62);	
-				couponCode+=chars[random%chars.length];
-				j++;
-			}	
-			//String couponCode=sb.toString();
-			System.out.println(i+"th Coupon Code: "+couponCode);
+public class Day6Assignment {
+
+	void perfect(int a) {
+		int sum=0;
+		for(int i=1; i<a; i++) {
+			if(a%i == 0) {
+				sum+=i;
+			}
 		}
+
+		if(sum==a) 
+			System.out.println(a +" This is a perfect number");
+
+		else
+			System.out.println(a +" This is not a perfect number");
+	}
+
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		Day6Assignment A = new Day6Assignment();
+		A.perfect(a);
 	}
 }
